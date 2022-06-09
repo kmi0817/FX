b278d915cd9872c3f0729b2890153c400e15bc9c
kmi0817 <kmi0817@naver.com>
2022-06-09 20:02:20 +0900

    feat: 생성일 필드 추가

--- src/Certificate.java
+++ src/Certificate.java
@@ -1,10 +1,14 @@
+import java.util.Date;
+
 public class Certificate {
+    private Date createDate; // 생성일
     private Deal deal;
-    private Settlements settlements; // 승인자
+    private Settlements settlements; // 승인인
     private String bankDealDate; // 은행 거래일
     private String billSettlementDate; // 어음결제일
 
     Certificate(Deal deal, Settlements settlements) {
+        this.createDate = new Date();
         this.deal = deal;
         this.settlements = settlements;
         this.bankDealDate = "";
