0fee57da0f57bac84b0f0cb89cdfe67f14b8a04b
kmi0817 <kmi0817@naver.com>
2022-06-09 14:53:29 +0900

    feat: Counterparty 클래스

---
+++ src/Counterparty.java
@@ -0,0 +1,11 @@
+public class Counterparty {
+    private String name;
+    private int positionLimits;
+    private String contactInfo;
+
+    Counterparty(String name, int positionLimits, String contactInfo) {
+        this.name = name;
+        this.positionLimits = positionLimits;
+        this.contactInfo = contactInfo;
+    }
+}
